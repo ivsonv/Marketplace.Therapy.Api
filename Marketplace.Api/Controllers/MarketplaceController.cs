7a0a4c74336fadf317c236ca6a022fcae4cf856f
IVSON VITAL <ivsonv@gmail.com>
2021-08-13 14:10:51 -0300

    remove coments

--- Marketplace.Api/Controllers/MarketplaceController.cs
+++ Marketplace.Api/Controllers/MarketplaceController.cs
@@ -21,24 +21,5 @@ namespace Marketplace.Api.Controllers
         [HttpGet]
         public async Task<BaseRs<List<providerMktRs>>> Show([FromQuery] providerMktRq _request)
             => await _marketplaceService.ShowProviders(_request);
-
-        //[HttpGet("{id:int}")]
-        //public async Task<BaseRs<bankRs>> FindById([FromRoute] int id)
-        //    => await _bankService.FindById(id);
-
-        //[HttpPost]
-        //public async Task<BaseRs<bankRs>> Store([FromBody] BaseRq<bankRq> _request)
-        //    => await _bankService.Store(_request);
-
-        //[HttpPut]
-        //public async Task<BaseRs<bankRs>> Update([FromBody] BaseRq<bankRq> _request)
-        //    => await _bankService.Update(_request);
-
-        //[HttpDelete("{id:int}")]
-        //public async Task<BaseRs<bool>> Delete([FromRoute] int id)
-        //    => await _bankService.Delete(id);
-
-        //[HttpGet("account-types")]
-        //public dynamic ShowSituations() => _bankService.getAccountTypes();
     }
 }
